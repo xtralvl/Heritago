31a917a40cdcafe9bf36a2a8098fcfb8fdbf4705
Roland Szucs <rolandszucs.private@gmail.com>
2025-10-31 18:08:00 +0100

    her-and-option-buttons-style-mobile

---
+++ my-app/src/App.tsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import './styles/App.scss';
+import HomePage from './views/HomePage';
+
+export default function App() {
+  return <HomePage />
+};
---
+++ my-app/src/views/HomePage.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import HomeNavAndHero from "../components/homePageComponents/HomeNavAndHero";
+
+export default function HomePage() {
+    return (
+        <>
+        <HomeNavAndHero />
+
+        </>
+    )
+};
