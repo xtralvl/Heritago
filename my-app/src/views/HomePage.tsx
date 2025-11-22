e41a0b2a309b89c938fe9cdd619c16e888956ab9
Roland Szucs <rolandszucs.private@gmail.com>
2025-11-22 13:47:37 +0100

    hide homepage overflow x

--- my-app/src/views/HomePage.tsx
+++ my-app/src/views/HomePage.tsx
@@ -1,16 +1,18 @@
 import HomeNavAndHero from "../components/homePageComponents/HomeNavAndHero";
 import HomeBodyAndFooter from "../components/homePageComponents/HomeBodyAndFooter";
+import '../styles/homePageStyles/HomePage.scss'
 
 
 export default function HomePage() {
 
 
     return (
-        <>
+    
+        <div className="homepage" >
         <HomeNavAndHero
         />
         <HomeBodyAndFooter
         />
-        </>
+        </div>
     )
 };
